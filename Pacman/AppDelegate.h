a4e2a150e98f4e43f1a660d7ba0a5ef5661c0620
Olesya <olesya.bolovintseva@gmail.com>
2013-02-11 12:51:56 +0400

    Initial Commit

---
+++ Pacman/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Pacman
+//
+//  Created by Администратор on 2/11/13.
+//  Copyright (c) 2013 Olesya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pacman/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pacman
+//
+//  Created by Администратор on 2/11/13.
+//  Copyright (c) 2013 Olesya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
